d350425270f71ad6dc373db39e7e9176f28f6695
Mohamadou Hamza <mohamadouhamza983@gmail.com>
2026-09-11 15:00:31 +0100

    Update

--- android_old/app/build.gradle.kts
+++ android_old/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.gestion_betail"
+    namespace = "com.berger.gestion_betail"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.gestion_betail"
+        applicationId = "com.berger.gestion_betail"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
